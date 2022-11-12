c77513386d7bd3ad666bc2c6a23f1dfe7418766f
kionou <kionoumamadou.00@gmail.com>
2022-11-12 16:33:19 +0000

    ok

---
+++ src/components/cardCathegorie.vue
@@ -0,0 +1,43 @@
+<template>
+  
+                <div class="card">
+                    <p>Gateaux</p>
+                </div>
+
+             
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* caerd debut */
+
+
+ .card{
+    border: 1px solid var(--vert);
+    width: 150px;
+    padding: 20px;
+    border-radius: 8px;
+    color: var(--vert) ;
+}
+.card:hover{
+    background-color: var(--vert);
+    color: var(--blanc) !important;
+    cursor: pointer;
+}
+@media (max-width:768px) {
+ .card{
+        padding: 10px;
+        width: 100px;
+    }
+
+}
+
+/* caerd finnnnn */
+
+</style>
--- src/views/Gateaux.vue
+++ src/views/Gateaux.vue
@@ -2,6 +2,13 @@
   <div>
     <div class="container-cathegorie">
         <div class="cathegorie">
+            <h1>NOS GATEAUX</h1>
+            <div class="cathegorie-card">
+                <Card v-for="(card , index) in 7" :key="index"/>
+            </div>
+
+        </div>
+        <div class="article">
 
         </div>
 
@@ -10,7 +17,13 @@
 </template>
 
 <script>
+import Card from '@/components/cardCathegorie.vue';
+
 export default {
+    name:'"Gateaux"',
+    components:{
+        Card
+    }
 
 }
 </script>
@@ -19,14 +32,33 @@ export default {
 .container-cathegorie{
     border: 1px solid red;
     width: 100%;
-    height: 30vh;
+    height: auto;
 }
 .cathegorie{
     max-width: var(--max-width);
     border: 1px solid blue;
     margin: 0 auto;
+    height: auto;
+   
+}
+.cathegorie .cathegorie-card {
+    display: flex;
+    grid-gap: 26px;
+    margin: 0 auto;
+    padding: 40px 0;
+    width: 100%;
+    border: 1px solid red;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.article{
+    max-width: var(--max-width);
+    border: 1px solid rgb(0, 255, 140);
+    margin: 20px auto;
     height: 20vh;
    
 }
+
 
 </style>
